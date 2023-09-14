1c51b5064f9a4e8fc0c5966993fa5dff46469ee9
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-13 21:04:31 -0400

    Initial commit

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="sheet-template">
+            <div className="print-typesand-logos">
+              <div className="options-change">
+                <div className="overlap-group">
+                  <img className="image" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+                  <div className="div-wrapper">
+                    <div className="text-wrapper">Print Type 1*</div>
+                  </div>
+                </div>
+              </div>
+              <div className="overlap-wrapper">
+                <div className="overlap-2">
+                  <img className="img" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-1.svg" />
+                  <div className="div-wrapper">
+                    <div className="text-wrapper">Print Type 2</div>
+                  </div>
+                </div>
+              </div>
+              <img
+                className="logo-dragor-clickto"
+                alt="Logo dragor clickto"
+                src="https://c.animaapp.com/3HPLS3ec/img/logodragorclicktoupload1@2x.png"
+              />
+              <img
+                className="logo-dragor-clickto-2"
+                alt="Logo dragor clickto"
+                src="https://c.animaapp.com/3HPLS3ec/img/logodragorclicktoupload1@2x.png"
+              />
+              <img
+                className="logo-dragor-clickto-3"
+                alt="Logo dragor clickto"
+                src="https://c.animaapp.com/3HPLS3ec/img/logodragorclicktoupload1@2x.png"
+              />
+              <img
+                className="logo-dragor-clickto-4"
+                alt="Logo dragor clickto"
+                src="https://c.animaapp.com/3HPLS3ec/img/logodragorclicktoupload1@2x.png"
+              />
+              <div className="canvas-for-dragging" />
+            </div>
+          </div>
+          <div className="color-dropdown">
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <img className="image-2" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+                <div className="div-wrapper">
+                  <div className="text-wrapper">Color*</div>
+                </div>
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-3">
+                <img className="image-3" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+                <div className="div-wrapper">
+                  <div className="text-wrapper">Color</div>
+                </div>
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-3">
+                <img className="image-3" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+                <div className="div-wrapper">
+                  <div className="text-wrapper">Color</div>
+                </div>
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-3">
+                <img className="image-3" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+                <div className="div-wrapper">
+                  <div className="text-wrapper">Color</div>
+                </div>
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-3">
+                <img className="image-3" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+                <div className="div-wrapper">
+                  <div className="text-wrapper">Color</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="options-saveaspdf">
+          <div className="overlap-4">
+            <img className="image-4" alt="Image" src="https://c.animaapp.com/3HPLS3ec/img/image-7.svg" />
+            <div className="div-2">
+              <div className="text-wrapper-2">Save PDF</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-5">
+          <div className="page">
+            <div className="div-placeholder">
+              <div className="text-wrapper-3">1</div>
+            </div>
+          </div>
+          <div className="div-placeholder-wrapper">
+            <div className="div-placeholder">
+              <div className="text-wrapper-3">2</div>
+            </div>
+          </div>
+          <div className="selectedpage">
+            <div className="div-placeholder">
+              <div className="text-wrapper-3">3</div>
+            </div>
+          </div>
+          <div className="add-another-page">
+            <div className="text-wrapper-4">+</div>
+          </div>
+        </div>
+        <div className="salesforce-PROJ">
+          <div className="overlap-6">
+            <div className="projnumbersalesforce">
+              <div className="proj-wrapper">
+                <input className="proj" />
+              </div>
+            </div>
+            <img
+              className="span-bg-light"
+              alt="Span bg light"
+              src="https://c.animaapp.com/3HPLS3ec/img/span-bg-light.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
